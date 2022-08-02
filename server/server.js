7d4f54cb122bc34ff9ae1a673c96daeb4b9cfb3c
Max Fathauer <maxfathauer@gmail.com>
2022-08-02 17:00:23 -0400

    connectiong to mongodb set up

--- server/server.js
+++ server/server.js
@@ -4,3 +4,36 @@ const db = require('./config/connection');
 const {ApolloServer} = require('appolo-server-exress');
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+async function startApolloServer() {
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
+    await server.start();
+  
+    server.applyMiddleware({ app });
+  
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  
+  
+  
+    db.once("once", () => {
+      app.listen(PORT, () => console.log('Now listening on localhost:${PORT}'));
+    })
+  
+    return {server, app};
+  }
+
+  startApolloServer(typeDefs, resolvers);
