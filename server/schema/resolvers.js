2c89c101360e20fe5f5b5862193223764188c91c
Max Fathauer <maxfathauer@gmail.com>
2022-08-03 21:47:49 -0400

    started schema set up and added more npm packets

---
+++ server/schema/resolvers.js
@@ -0,0 +1,13 @@
+const {User} =  require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers ={
+    Query:{
+
+    },
+
+    Mutation:{
+        
+    }
+}
